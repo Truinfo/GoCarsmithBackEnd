305bbccf058196e81f795d0bc06b94b6a607acb4
TrueInfo <harish.jammu@outlook.com>
2024-01-29 18:41:45 +0530

    SecondChange

--- src/index.server.js
+++ src/index.server.js
@@ -162,5 +162,7 @@ mongoose.set('strictQuery', false);
 
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port  ${process.env.PORT}`);
 });
+
+
