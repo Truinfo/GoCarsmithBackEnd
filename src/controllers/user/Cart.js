8703e6b469446cbe4b6f381db3d18a1628859bfb
TrueInfo <harish.jammu@outlook.com>
2024-01-29 16:01:45 +0530

    Change

--- src/controllers/user/Cart.js
+++ src/controllers/user/Cart.js
@@ -1,4 +1,4 @@
-const AddToCartModel = require("../../models/user/cART.JS")
+const AddToCartModel=require('../../models/user/Cart')
 const mongoose = require('mongoose')
 
 
@@ -67,74 +67,76 @@ exports.AddItemsToCart = async (req, res) => {
     console.error(error);
   }
 };
-exports.getCartItemsByUserId = async (req, res) => {
-    const { userId } = req.params
-    try {
-        const getuserCart = await AddToCartModel.find({ userId })
-        if (!getuserCart) {
-            res.status(401).json({ message: "data not found" })
-        }
-        res.json(getuserCart)
 
-    } catch (error) {
-        res.status(500).json(error);
-        console.log(error)
-    }
+
+exports.getCartItemsByUserId = async (req, res) => {
+  const { userId } = req.params
+  try {
+      const getuserCart = await AddToCartModel.find({ userId })
+      if (!getuserCart) {
+          res.status(401).json({ message: "data not found" })
+      }
+      res.json(getuserCart)
+
+  } catch (error) {
+      res.status(500).json(error);
+      console.log(error)
+  }
 }
 
 exports.deleteCart = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const itemId = req.params.itemId;
-    // Assuming you have a field like 'userId' in your CartItem schema
-    const result = await AddToCartModel.deleteOne({ _id: itemId, userId: userId });
-    if (result.deletedCount > 0) {
-      res.status(200).json({ message: 'CartItem deleted successfully' });
-    } else {
-      res.status(404).json({ message: 'CartItem not found or already deleted' });
-    }
-  } catch (error) {
-    console.error('Error deleting cart item:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+try {
+  const userId = req.params.userId;
+  const itemId = req.params.itemId;
+  // Assuming you have a field like 'userId' in your CartItem schema
+  const result = await AddToCartModel.deleteOne({ _id: itemId, userId: userId });
+  if (result.deletedCount > 0) {
+    res.status(200).json({ message: 'CartItem deleted successfully' });
+  } else {
+    res.status(404).json({ message: 'CartItem not found or already deleted' });
   }
+} catch (error) {
+  console.error('Error deleting cart item:', error);
+  res.status(500).json({ error: 'Internal Server Error' });
+}
 };
 exports.RemoveCartItemById = async (req, res) => {
-  const { userId, itemId } = req.params;
-  try {
-    const result = await AddToCartModel.findOneAndUpdate(
-      { userId },
-      { $pull: { listOfServices: { _id: mongoose.Types.ObjectId(itemId) } } },
-      { new: true }
-    );
-    if (!result) {
-      return res.status(404).json({ message: 'User not found or item not in the cart' });
-    }
-    res.json({ message: 'Item deleted successfully', updatedCart: result });
-  } catch (error) {
-    console.error('Error deleting item from cart:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+const { userId, itemId } = req.params;
+try {
+  const result = await AddToCartModel.findOneAndUpdate(
+    { userId },
+    { $pull: { listOfServices: { _id: mongoose.Types.ObjectId(itemId) } } },
+    { new: true }
+  );
+  if (!result) {
+    return res.status(404).json({ message: 'User not found or item not in the cart' });
   }
+  res.json({ message: 'Item deleted successfully', updatedCart: result });
+} catch (error) {
+  console.error('Error deleting item from cart:', error);
+  res.status(500).json({ error: 'Internal Server Error' });
+}
 };
 
 exports.clearCartInDatabase = async (req,res) => {
-  const {userId}=req.params
-  try {
-    // Find and remove the cart for the given user
-    const removedCart = await AddToCartModel.findOneAndRemove({ userId });
-    if (!removedCart) {
-      return res.status(404).json({ message: "Cart not found for the user" });
-    }
-    res.json({ message: "Cart cleared successfully" });
-  } catch (error) {
-    console.error("Error clearing cart:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+const {userId}=req.params
+try {
+  // Find and remove the cart for the given user
+  const removedCart = await AddToCartModel.findOneAndRemove({ userId });
+  if (!removedCart) {
+    return res.status(404).json({ message: "Cart not found for the user" });
   }
+  res.json({ message: "Cart cleared successfully" });
+} catch (error) {
+  console.error("Error clearing cart:", error);
+  res.status(500).json({ error: "Internal Server Error" });
+}
 };
 
 
 // exports.UpdateCartItems=async(req,res)=>{
 //     const { userId, itemId } = req.params;
-  
+
 // try {
 //   const result = await AddToCartModel.updateOne(
 //     { userId },
